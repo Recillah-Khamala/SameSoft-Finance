ff381a73d3890e1b9b8afb1dcf2fe52a604ca871
recillah khamala <recillahk@gmail.com>
2024-10-02 17:34:35 +0300

    Handle package.json error

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  extends: 'react-app',
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  rules: {
+    // You can add custom rules here if needed
+  },
+};
